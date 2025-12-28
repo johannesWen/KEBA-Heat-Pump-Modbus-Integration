2eddfb3420be8382bc6df0543c5b9dd54c02cac0
Johannes Wenninger <57130737+johannesWen@users.noreply.github.com>
2025-12-28 14:36:48 +0100

    Handle climate platform fallback

--- custom_components/keba_heat_pump_modbus/const.py
+++ custom_components/keba_heat_pump_modbus/const.py
@@ -8,6 +8,11 @@ try:
 except AttributeError:
     WATER_HEATER_PLATFORM = "water_heater"
 
+try:
+    CLIMATE_PLATFORM = Platform.CLIMATE
+except AttributeError:
+    CLIMATE_PLATFORM = "climate"
+
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_UNIT_ID = "unit_id"
@@ -28,7 +33,7 @@ PLATFORMS = [
     Platform.BINARY_SENSOR,
     Platform.NUMBER,
     Platform.SELECT,
-    Platform.CLIMATE,
+    CLIMATE_PLATFORM,
     WATER_HEATER_PLATFORM,
 ]
 
